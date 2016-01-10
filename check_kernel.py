8e5d45f8344e33bd52539166b4cd5f9c48c61636
Anderson Fachini <anderson.dkfachini@gmail.com>
2016-01-10 13:14:00 -0200

    Adjusted style and formatting; finalized first version

--- check_kernel.py
+++ check_kernel.py
@@ -3,37 +3,41 @@ import re
 import subprocess
 import os
 import hashlib
-import io
 
-def getSiteContent(url, joinLines = True):
+
+def get_site_content(url, joinLines=True):
     print('Checking %s...' % url)
 
     resp = urllib.request.urlopen(url).read()
 
     content = resp.decode('utf-8')
-    content = content.replace('\t', '').replace('  ', '')
     if joinLines:
-      content = content.replace('\r', '').replace('\n', '')
+        content = content.replace('\t', '').replace('  ', '')
+        content = content.replace('\r', '').replace('\n', '')
 
     return content
 
-def findInContent(content, regex):
+
+def find_in_content(content, regex):
     p = re.compile(regex)
 
     return p.findall(content)
 
-def getSiteVersion():
-    content = getSiteContent('https://www.kernel.org')
 
-    return findInContent(content, '<td id="latest_link"><a.*?>(.*?)</a>')[0]
+def get_site_version():
+    content = get_site_content('https://www.kernel.org')
 
-def executeSystemCommand(commandList):
+    return find_in_content(content, '<td id="latest_link"><a.*?>(.*?)</a>')[0]
+
+
+def execute_system_command(commandList):
     ret = subprocess.run(commandList, stdout=subprocess.PIPE)
 
     return ret.stdout
 
-def getInstalledVersion():
-    ret = executeSystemCommand(["uname", "-r"])
+
+def get_installed_version():
+    ret = execute_system_command(["uname", "-r"])
     instVersion = str(ret)[2:-4].split('-')[0]
 
     if instVersion.endswith('.0'):
@@ -41,19 +45,22 @@ def getInstalledVersion():
 
     return instVersion
 
-def adjustVersion(version, addDigit=True):
+
+def ajust_version(version, addDigit=True):
     if addDigit and len(version.split('.')) == 2:
         version += '.0'
 
     return version.replace('.', '\.')
 
-def getUbuntuSiteVersions(version):
-    content = getSiteContent('http://kernel.ubuntu.com/~kernel-ppa/mainline/')
-    regex = '"v({}-[^/]*)'.format( adjustVersion(version, False) )
 
-    return findInContent(content, regex)[::-1]
+def get_ubuntu_site_version(version):
+    content = get_site_content('http://kernel.ubuntu.com/~kernel-ppa/mainline/')
+    regex = '"v({}-[^/]*)'.format(ajust_version(version, False))
 
-def getNumberedList(list_):
+    return find_in_content(content, regex)[::-1]
+
+
+def get_numbered_list(list_):
     newList = []
 
     for i, item in enumerate(list_):
@@ -61,61 +68,86 @@ def getNumberedList(list_):
 
     return newList
 
-def getDownloadLinks(version, versionLink):
-    content = getSiteContent('http://kernel.ubuntu.com/~kernel-ppa/mainline/v' + versionLink)
 
-    regex = '"(linux-(?:headers|image)-%s.{7}(?:.{26}_all|-generic.{26}_amd64)\.deb)' % adjustVersion(version)
+def get_download_file_names(version, versionLink):
+    link = 'http://kernel.ubuntu.com/~kernel-ppa/mainline/v' + versionLink
+    content = get_site_content(link)
 
-    return findInContent(content, regex)
+    regex = '"(linux-(?:headers|image)-%s.{7}(?:.{26}_all|-generic.{26}_amd64)\.deb)' % ajust_version(version)
 
-def getFiles(files, versionLink):
+    return find_in_content(content, regex)
+
+
+def get_files(files, versionLink):
+    prefix_url = 'http://kernel.ubuntu.com/~kernel-ppa/mainline'
+
     for i, f in enumerate(files):
-        files[i] = '{0}/v{1}/{2}'.format('http://kernel.ubuntu.com/~kernel-ppa/mainline', versionLink, f)
+        files[i] = '{0}/v{1}/{2}'.format(prefix_url, versionLink, f)
 
-    executeSystemCommand(['wget'] + files)
+    execute_system_command(['wget'] + files)
 
-def removeFiles():
-    executeSystemCommand(['rm', 'linux*.deb'])
 
-def installPackages():
-    executeSystemCommand(['sudo', 'dpkg', '-i', 'linux-headers*.deb', 'linux-image*.deb'])
-
-def checkForExistingDebFiles():
+def remove_files(file_names):
     current_path = os.getcwd()
-    files = []
 
-    for file in os.listdir(current_path):
-      if file.endswith('.deb'):
-        files.push(file)
+    params = ['rm']
+    params.extend(file_names)
+
+    execute_system_command(params)
+
+
+def install_packages(file_names):
+    current_path = os.getcwd()
+
+    params = ['sudo', 'dpkg', '-i']
+    params.extend(file_names)
+
+    execute_system_command(params)
+
+
+def check_existing_deb_files(file_names):
+    current_path = os.getcwd()
+
+    files = [file for file in os.listdir(current_path) if file in file_names]
 
     return files
 
-# http://stackoverflow.com/questions/1869885/calculating-sha1-of-a-file 
-def sha1OfFile(filepath):
+
+# http://stackoverflow.com/questions/1869885/calculating-sha1-of-a-file
+def sha1_of_file(filepath):
     import hashlib
     with open(filepath, 'rb') as f:
         return hashlib.sha1(f.read()).hexdigest()
 
-def getSiteChecksums(version):
-    link = 'http://kernel.ubuntu.com/~kernel-ppa/mainline/v' + version + '/CHECKSUMS'
-    content = getSiteContent(link, False)
 
-    regex = '\n(.{40})\s{2}linux-(?:headers|image)-.{12}(?:.{26}_all|-generic.{26}_amd64)\.deb'
+def get_site_checksums(version):
+    link = 'http://kernel.ubuntu.com/~kernel-ppa/mainline/v%s/CHECKSUMS' % version
+    content = get_site_content(link, False)
 
-    return findInContent(content, regex)
+    regex = '\n(.{40})\s{2}(linux-(?:headers|image)-.{12}(?:.{26}_all|-generic.{26}_amd64)\.deb)'
 
-siteVersion = getSiteVersion()
-instVersion = getInstalledVersion()
+    finds = find_in_content(content, regex)
+    dict = {}
+
+    for item in finds:
+        dict[item[1]] = item[0]
+
+    return dict
+
+
+siteVersion = get_site_version()
+instVersion = get_installed_version()
 
 if siteVersion == instVersion:
     askContinue = True
-    resp = input('Installed version (%s) is the same of last available. Update anyway [N/y]? ' % instVersion)
+    resp = input('Installed version (%s) is the same of last available.\
+        Update anyway [N/y]? ' % instVersion)
 
     if not resp or resp == 'N':
         exit()
 else:
-    print( 'Site kernel: %s' % siteVersion )
-    print( 'Installed kernel: %s' % instVersion )
+    print('Installed kernel: %s' % instVersion)
+    print('Site kernel: %s' % siteVersion)
 
     resp = input('Would you like do update to %s (Y/n): ' % siteVersion)
 
@@ -123,12 +155,12 @@ else:
         exit()
 
 print('Checking available versions...')
-versions = getUbuntuSiteVersions(siteVersion)
-versions = getNumberedList(versions)
+versions = get_ubuntu_site_version(siteVersion)
+versions = get_numbered_list(versions)
 
-print( 'Versions found:\n%s' % '\n'.join(versions) )
+print('Versions found:\n%s' % '\n'.join(versions))
 
-version = input( 'Wich version do you want to install [1]: ' )
+version = input('Wich version do you want to install [1]: ')
 if not version:
     index = 0
 else:
@@ -136,13 +168,25 @@ else:
 
 linkVersion = versions[index].split(' - ')[1]
 
-links = getDownloadLinks(siteVersion, linkVersion)
+file_names = get_download_file_names(siteVersion, linkVersion)
 
-print('Obtaining files...')
-getFiles(links, linkVersion)
+existing_files = check_existing_deb_files(file_names)
+
+if existing_files:
+    checksums = get_site_checksums(linkVersion)
+
+    for file_name in existing_files:
+        if sha1_of_file(file_name) != checksums[file_name]:
+            existing_files -= file_name
+
+files_left = [file for file in file_names if file not in existing_files]
+
+if len(files_left):
+    print('Obtaining files...')
+    get_files(files_left, linkVersion)
 
 print('Installing packages...')
-installPackages()
+install_packages(file_names)
 
 print('Remmoving files...')
-removeFiles()
+remove_files(file_names)
